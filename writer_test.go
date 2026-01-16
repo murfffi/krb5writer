cace09ea48dc04d27ecee9774ca7b538fdc1dcfe
Marin Nozhchev <murfffi@gmail.com>
2026-01-16 09:49:08 +0200

    Initial implementation (#1)

---
+++ writer_test.go
@@ -0,0 +1,31 @@
+package template
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jcmturner/gokrb5/v8/config"
+	"github.com/jcmturner/krb5test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHello(t *testing.T) {
+	l := log.New(os.Stderr, "KDC Test Server: ", log.LstdFlags)
+	p := make(map[string][]string)
+	p["testuser1"] = []string{"testgroup1"}
+	p["HTTP/host.test.realm.com"] = []string{}
+	kdc, err := krb5test.NewKDC(p, l)
+	require.NoError(t, err)
+	kdc.Start()
+	defer kdc.Close()
+	conf := kdc.KRB5Conf
+	t.Logf("%+v", conf)
+	krbConfFile := t.TempDir() + "/krb5.conf"
+	err = WriteKrb5Conf(conf, krbConfFile)
+	require.NoError(t, err)
+
+	loadedConf, err := config.Load(krbConfFile)
+	require.NoError(t, err)
+	require.Equal(t, conf, loadedConf)
+}
