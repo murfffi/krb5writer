8870590fe4f3b4b164824a718764d7c385606dc1
Marin Nozhchev <murfffi@gmail.com>
2026-01-16 09:20:57 +0200

    Initial implementation

---
+++ writer.go
@@ -0,0 +1,157 @@
+package template
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/jcmturner/gokrb5/v8/config"
+)
+
+func WriteKrb5Conf(config *config.Config, path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// [libdefaults]
+	fmt.Fprintln(f, "[libdefaults]")
+	ld := config.LibDefaults
+	writeBool(f, "allow_weak_crypto", ld.AllowWeakCrypto)
+	writeBool(f, "canonicalize", ld.Canonicalize)
+	if ld.CCacheType != 0 {
+		writeIndentedLine(f, 1, "ccache_type = %d", ld.CCacheType)
+	}
+	if ld.Clockskew != 0 {
+		writeIndentedLine(f, 1, "clockskew = %s", ld.Clockskew)
+	}
+	writeString(f, "default_client_keytab_name", ld.DefaultClientKeytabName)
+	writeString(f, "default_keytab_name", ld.DefaultKeytabName)
+	writeString(f, "default_realm", ld.DefaultRealm)
+	writeStringSlice(f, "default_tgs_enctypes", ld.DefaultTGSEnctypes)
+	writeStringSlice(f, "default_tkt_enctypes", ld.DefaultTktEnctypes)
+	writeBool(f, "dns_canonicalize_hostname", ld.DNSCanonicalizeHostname)
+	writeBool(f, "dns_lookup_kdc", ld.DNSLookupKDC)
+	writeBool(f, "dns_lookup_realm", ld.DNSLookupRealm)
+
+	if len(ld.ExtraAddresses) > 0 {
+		var s []string
+		for _, ip := range ld.ExtraAddresses {
+			s = append(s, ip.String())
+		}
+		writeIndentedLine(f, 1, "extra_addresses = %s", strings.Join(s, ","))
+	}
+
+	writeBool(f, "forwardable", ld.Forwardable)
+	writeBool(f, "ignore_acceptor_hostname", ld.IgnoreAcceptorHostname)
+	writeBool(f, "k5login_authoritative", ld.K5LoginAuthoritative)
+	writeString(f, "k5login_directory", ld.K5LoginDirectory)
+
+	if len(ld.KDCDefaultOptions.Bytes) > 0 {
+		writeIndentedLine(f, 1, "kdc_default_options = 0x%s", hex.EncodeToString(ld.KDCDefaultOptions.Bytes))
+	}
+
+	if ld.KDCTimeSync != 0 {
+		writeIndentedLine(f, 1, "kdc_timesync = %d", ld.KDCTimeSync)
+	}
+
+	writeBool(f, "noaddresses", ld.NoAddresses)
+	writeStringSlice(f, "permitted_enctypes", ld.PermittedEnctypes)
+
+	if len(ld.PreferredPreauthTypes) > 0 {
+		var s []string
+		for _, v := range ld.PreferredPreauthTypes {
+			s = append(s, fmt.Sprintf("%d", v))
+		}
+		writeIndentedLine(f, 1, "preferred_preauth_types = %s", strings.Join(s, ","))
+	}
+
+	writeBool(f, "proxiable", ld.Proxiable)
+	writeBool(f, "rdns", ld.RDNS)
+
+	// RealmTryDomains default -1.
+	if ld.RealmTryDomains != -100 { // Hack: relying on 0 being possibly valid? The parser defaults to -1.
+		// If it is -1, do we write it? Yes.
+		writeIndentedLine(f, 1, "realm_try_domains = %d", ld.RealmTryDomains)
+	}
+
+	if ld.RenewLifetime != 0 {
+		writeIndentedLine(f, 1, "renew_lifetime = %s", ld.RenewLifetime)
+	}
+
+	if ld.SafeChecksumType != 0 {
+		writeIndentedLine(f, 1, "safe_checksum_type = %d", ld.SafeChecksumType)
+	}
+
+	if ld.TicketLifetime != 0 {
+		writeIndentedLine(f, 1, "ticket_lifetime = %s", ld.TicketLifetime)
+	}
+
+	if ld.UDPPreferenceLimit != 1465 { // Default is 1465 in LibDefaults?
+		// In newLibDefaults it is 1465.
+		// We should probably write it whatever it is.
+		writeIndentedLine(f, 1, "udp_preference_limit = %d", ld.UDPPreferenceLimit)
+	}
+	writeBool(f, "verify_ap_req_nofail", ld.VerifyAPReqNofail)
+
+	// [realms]
+	fmt.Fprintln(f, "\n[realms]")
+	for _, r := range config.Realms {
+		writeIndentedLine(f, 1, "%s = {", r.Realm)
+		for _, v := range r.AdminServer {
+			writeIndentedLine(f, 2, "admin_server = %s", v)
+		}
+		if r.DefaultDomain != "" {
+			writeIndentedLine(f, 2, "default_domain = %s", r.DefaultDomain)
+		}
+		for _, v := range r.KDC {
+			writeIndentedLine(f, 2, "kdc = %s", v)
+		}
+		for _, v := range r.KPasswdServer {
+			// Parser defaults kpasswd_server to admin_server:464 if missing.
+			// But if it is present, write it.
+			writeIndentedLine(f, 2, "kpasswd_server = %s", v)
+		}
+		for _, v := range r.MasterKDC {
+			writeIndentedLine(f, 2, "master_kdc = %s", v)
+		}
+		writeIndentedLine(f, 1, "}")
+	}
+
+	// [domain_realm]
+	fmt.Fprintln(f, "\n[domain_realm]")
+	var domains []string
+	for d := range config.DomainRealm {
+		domains = append(domains, d)
+	}
+	sort.Strings(domains)
+	for _, d := range domains {
+		writeIndentedLine(f, 1, "%s = %s", d, config.DomainRealm[d])
+	}
+
+	return nil
+}
+
+func writeIndentedLine(w io.Writer, indent int, format string, a ...interface{}) {
+	fmt.Fprintf(w, strings.Repeat("\t", indent)+format+"\n", a...)
+}
+
+func writeBool(w io.Writer, key string, v bool) {
+	writeIndentedLine(w, 1, "%s = %t", key, v)
+}
+
+func writeString(w io.Writer, key string, v string) {
+	if v != "" {
+		writeIndentedLine(w, 1, "%s = %s", key, v)
+	}
+}
+
+func writeStringSlice(w io.Writer, key string, v []string) {
+	if len(v) > 0 {
+		writeIndentedLine(w, 1, "%s = %s", key, strings.Join(v, " "))
+	}
+}
